4dde09bb98945f83d6edddba8f3c9ddaa3f5d661
LoveAnn <LoveAnn@Admins-MacBook.local>
2019-11-14 11:15:02 +0800

    Updates

---
+++ precache-manifest.3ce4aee4d28c0a066cd41862f1a21b1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70c00ffd6c725f4cf43d",
+    "url": "/reactjs-awsreko-demo/static/css/main.2265d451.chunk.css"
+  },
+  {
+    "revision": "70c00ffd6c725f4cf43d",
+    "url": "/reactjs-awsreko-demo/static/js/main.70c00ffd.chunk.js"
+  },
+  {
+    "revision": "164bcb12c81c1d395e49",
+    "url": "/reactjs-awsreko-demo/static/js/1.164bcb12.chunk.js"
+  },
+  {
+    "revision": "3a8ae5e804c1bcc7a771",
+    "url": "/reactjs-awsreko-demo/static/js/runtime~main.3a8ae5e8.js"
+  },
+  {
+    "revision": "5ce979b98d7577ca0ece8901eeffa196",
+    "url": "/reactjs-awsreko-demo/index.html"
+  }
+];
